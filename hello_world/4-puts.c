3adb139bf7ddef70c36651ab2178f7c0afd6b9ec
Najwarci <rachdi.najwa91@gmail.com>
2025-02-17 13:55:50 +0100

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* return: Always end with value 0
+$
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
