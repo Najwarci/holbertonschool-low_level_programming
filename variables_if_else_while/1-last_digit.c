f014b9996a8e39ab581b08a5703d587a8a5f196c
Najwarci <rachdi.najwa91@gmail.com>
2025-02-18 15:49:51 +0100

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-	printf("Last digit of is greater than 5\n");
+	printf("Last digit of %d of is %d greater than 5\n", n, last_digit);
 	}
 	else if (last_digit == 0)
 	{
-	printf("Last digit of is 0\n");
+	printf("Last digit of %d of is %d and 0\n", n, last_digit);
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
-	printf("Last digit of is less than 6 and not 0\n");
+	printf("Last digit of %d of is %d less than 6 and not 0\n", n, last_digit);
 	}
 	printf("\n");
 
