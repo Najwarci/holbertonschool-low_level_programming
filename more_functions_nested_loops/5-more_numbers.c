f8cbcec127f844776eb9807db581fcba04ff7a7e
Najwarci <rachdi.najwa91@gmail.com>
2025-02-24 16:45:07 +0100

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (c = '0'; c <= 14; c++)
+		for (c = 0; c <= 14; c++)
 		{
-			_putchar(c);
+		if (c > 9)
+		_putchar('1');
+		_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
-	}
+		}
 }
