7c361e062123cde9df7e67bc8e2bfa2640421c1b
Najwarci <rachdi.najwa91@gmail.com>
2025-02-25 10:35:37 +0100

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,22 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n > 0)
 	{
 	for (i = 0; i < n; i++)
-		{
-		_putchar('\\');
-		}
-	for (i = 0; i < n; i++)
-		{
-		_putchar('\n');
-		}
+	{
+	for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
 	}
+	_putchar('\\');
 	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
