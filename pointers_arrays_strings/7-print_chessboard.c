29b15a62579feabef267f8b2813b58446b904c76
Najwarci <rachdi.najwa91@gmail.com>
2025-03-06 11:26:43 +0100

    7.2

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - Prints an 8x8 chessboard
+ * @a: The chessboard (2D array of characters)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = 0; j < 8; j++)
+	{
+	_putchar(a[i][j]);
+	}
+	putchar('\n');
+	}
+}
