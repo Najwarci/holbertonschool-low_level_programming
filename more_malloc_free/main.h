c6d65ae4a432b49300263d98be1adebe04e8fddb
Najwarci <rachdi.najwa91@gmail.com>
2025-03-14 16:28:48 +0100

    3

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -16,4 +16,8 @@ int **alloc_grid(int width, int height);
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+int *array_range(int min, int max);
+
+
 #endif
